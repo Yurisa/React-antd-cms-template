73fe1929c10547b628a280432154228840df3e0a
Yurisa <779509411@qq.com>
2018-09-13 19:10:36 +0800

    first-commit

---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import MenuConfig from '../../config/menuConfig'
+// import { NavLink } from 'react-router-dom'
+import { Menu } from 'antd'
+import './index.less'
+
+const SubMenu = Menu.SubMenu
+export default class NavLeft extends Component {
+    componentWillMount () {
+        const menuTreeNode = this.renderMenu(MenuConfig)
+
+        this.setState({
+            menuTreeNode
+        })
+    }
+
+    renderMenu = (data) => {
+        return data.map((item) => {
+            if (item.children) {
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        {this.renderMenu(item.children)}
+                    </SubMenu>
+                )
+            }
+            return (
+                <Menu.Item key={item.key}>
+                    {/* <NavLink to={item.key}>{item.title}</NavLink> */}
+                    {item.title}
+                </Menu.Item>
+            )
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div className="logo">
+                    <img src="/assets/logo-ant.svg" alt=""/>
+                    <h1>CMS</h1>
+                </div>
+                <Menu
+                    theme="dark"
+                >
+                    {this.state.menuTreeNode}
+                </Menu>
+                
+            </div>
+        )
+    }
+}
